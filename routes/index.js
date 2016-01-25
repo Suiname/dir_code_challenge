f010b86b1d20f6c6cbc14df72f1fc1047060942d
Jason Tham <jtham@sbcglobal.net>
2016-01-24 21:00:15 -0600

    added form, bootstrap, jquery

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Survey for WDI' });
 });
 
+router.get('/survey', function(req,res,next){
+  res.render('survey', { title: 'Survey for WDI' });
+})
+
 module.exports = router;
